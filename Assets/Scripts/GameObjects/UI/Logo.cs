864eb28e874ba686016ee469fb1c6c0b51b93ba4
kripsonud <kripsonud@gmail.com>
2020-08-30 14:46:32 +0100

    Add more effects to UI animtion

---
+++ Assets/Scripts/GameObjects/UI/Logo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Animator>().SetBool("IsPlaying", false);
+        Invoke("EnableAnimation", 3);
+    }
+
+    void EnableAnimation()
+    {
+        GetComponent<Animator>().SetBool("IsPlaying", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
