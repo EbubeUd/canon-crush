245930d646c4193f1ac0223a3dacce1e47d389dc
Precious Kosisochukwu <kcokolo10@gmail.com>
2020-08-30 02:23:25 +0100

    Add EffectManager script

---
+++ Assets/Scripts/Management/EffectManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+namespace Assets.Scripts.Management
+{
+    public class EffectManager
+    {
+        public static EffectManager Instance;
+
+        public GameObject ExplosionEffect;
+
+        private void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+        }
+
+        public void SpawnExplosionEffect(Vector3 location)
+        {
+            SpawnExplosionEffect(location);
+        }
+    }
+}
